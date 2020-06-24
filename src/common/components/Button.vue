515b7a439dabfb566f4e788f13b4c1ff564e9368
Akihiro Kondo <akihirokondo511@gmail.com>
2020-06-24 12:03:41 +0900

    add button actions

--- src/common/components/Button.vue
+++ src/common/components/Button.vue
@@ -1,11 +1,39 @@
 <template>
-  <button><slot /></button>
+  <button :disabled="isDisabled" @click="handleClick" class="button">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { _ } from '@/common'
 
 @Component
 export default class Button extends Vue {
+  @Prop() disabled?: boolean
+
+  get isDisabled(): boolean {
+    if (_.isUndefined(this.disabled)) {
+      return false
+    }
+    return this.disabled
+  }
+
+  handleClick(_event: Event) {
+    this.$emit('action')
+  }
 }
 </script>
+
+<style scoped>
+.button {
+  width: 300px;
+  margin: auto;
+  padding: 20px;
+  background-color: aqua;
+}
+.button[disabled] {
+  color: white;
+  background-color: gray;
+}
+</style>
--- src/loadingButton/LoadingButtonPage.vue
+++ src/loadingButton/LoadingButtonPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div>LoadingButtonPage</div>
-    <btn>loading button</btn>
+    <btn :disabled="isButtonDisabled" @action="handleAction">
+      {{ buttonLabel }}
+    </btn>
   </div>
 </template>
 
@@ -9,11 +11,37 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { Button } from '@/common'
 
+type Status = 'loading' | 'success' | 'failed'
+
 @Component({
   components: {
     btn: Button
   }
 })
 export default class LoadingButtonPage extends Vue {
+  status: Status = 'loading'
+
+  get isButtonDisabled(): boolean {
+    return this.status === 'loading'
+  }
+
+  get buttonLabel(): string {
+    if (this.status === 'success') {
+      return 'Click Me!!'
+    }
+    if (this.status === 'failed') {
+      return 'Please Retry'
+    }
+    return 'Now Loading...'
+  }
+
+  handleAction() {
+    if (this.status === 'success') {
+      alert('Success!!')
+    }
+    if (this.status === 'failed') {
+      location.reload()
+    }
+  }
 }
 </script>
