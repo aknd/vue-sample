9de3c5f14bb3b850b190f191bd1ed10bc6340a99
Akihiro Kondo <akihirokondo511@gmail.com>
2020-07-08 12:41:32 +0900

    rm unnessasary plugin for dayjs

--- src/common/datetime-util.ts
+++ src/common/datetime-util.ts
@@ -2,12 +2,10 @@ import dayjs, { Dayjs } from 'dayjs'
 import 'dayjs/locale/ja'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
-import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.locale('ja')
 dayjs.extend(isSameOrBefore)
 dayjs.extend(isSameOrAfter)
-dayjs.extend(relativeTime)
 
 export const buildDayjs = (date?: string | number | Dayjs | Date): Dayjs =>
   dayjs(date)
